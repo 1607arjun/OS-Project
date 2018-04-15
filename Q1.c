872618a6c9b1cf2387a681cc021c10f962310238
Arjun Sharma <37781435+1607arjun@users.noreply.github.com>
2018-04-15 23:22:26 +0530

    Update Q1.c
    fibonacci series ...

--- Q1.c
+++ Q1.c
@@ -1,30 +1,32 @@
 #include<pthread.h>
 #include<stdio.h>
-void *th_function1();
-void *th_function2();
+#include<unistd.h>
+void th_function1();
+void th_function2();
 int shared=0; // shared variable
 int main()
 {
+int x;
+printf("Enter the number of elememts -> ");
+scanf("%d",&x);
 pthread_t thread1, thread2;
 pthread_create(&thread1, NULL, th_function1, NULL);
-pthread_create(&thread2, NULL, th_function2, NULL);
+pthread_create(&thread2, NULL, th_function2, x);
 pthread_join(thread1, NULL);
 pthread_join(thread2,NULL);
-printf("Final value of shared is %d\n",shared); //prints the last updated value of shared variable
+printf("\nFinal value of shared variable is %d = number of elements in series.\n",shared); //prints the last updated value of shared variable
 }
 
-void *th_function2()
+void th_function2(int x)
 {
-int x;
+//x=shared;
      printf("Please enter a number:\n");
-    scanf("%d",&x);
-x=shared;    
+//x=shared;    
 shared=x ;
-     sleep(10);  //thread one is preempted by thread 2
  
 }
 
-void *th_function1()
+void th_function1()
 {
 //using shared make fib series
     printf("Fibonacci Series:  ");
@@ -32,13 +34,14 @@ int i;
 int t1=0;
 int t2=1;
 int nextTerm;
-
-    for (i = 1; i <= shared; ++i)
+int n=shared;
+printf("%d -> ",t1);
+printf("%d -> ",t2);
+    for (i = 1; i <=n; i++)
     {
-        printf("ertyui");
         nextTerm = t1 + t2;
         t1 = t2;
         t2 = nextTerm;
+printf("%d -> ",nextTerm);
     }
-printf("aaaaaaaaaaaaa");
 }
